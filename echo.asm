974da2465e4939b7781176f0ffd4327f496aad96
faissaloo <faissaloo@gmail.com>
2016-01-25 15:53:17 +0000

    Comment formatting

--- echo.asm
+++ echo.asm
@@ -6,46 +6,46 @@ _start:
     pop	ebx	        ; Pop the program name, we don't need this so we'll just overwrite it
 
     dec ebp
-    cmp ebp, 0
+    cmp ebp, 0      ; If there are no arguments just exit
     je _exit
     jmp _twoargs
 _twoargs:
     pop	ebx		      ; Get argument
     ;compare ebx with '-n' to see if they're the same
     cld
-    mov  ecx,2 ;'-n' will always be 2 characters long
+    mov  ecx,2      ; '-n' will always be 2 characters long
     lea  esi,[nlarg]
     lea  edi, [ebx]
     repe cmpsb
-    je _removenl ;If they are equal remove the newline
+    je _removenl    ;If they are equal remove the newline
     jmp _main
 _removenl:
     mov byte [newline],0 ;Removes the newline character from memory
-    pop	ebx		      ;Skips to the next argument
-    dec ebp         ;decrease the number of arguments left
+    pop	ebx		      ; Skips to the next argument
+    dec ebp         ; decrease the number of arguments left
     jmp _main
 _main:
-    cmp ebp, 0      ;If there is only one argument do nothing, just skip to the end
+    cmp ebp, 0      ; If there is only one argument do nothing, just skip to the end
     je _exit
     ;strlen(edi)
-    dec ebp        ;decrease the number of arguments left
+    dec ebp         ; Decrease the number of arguments left
     mov edi, ebx
     call _strlen
 
     ;Print the string
-    mov edx,eax     ;String length
-    mov ecx,ebx     ;String
-    mov ebx,1       ;stdout
-    mov eax,4       ;sys_write
-    int 0x80        ;Kernel interrupt
-    cmp ebp,0       ;Here we need to add a conditional to check if either the stack is empty or we've processed all arguments
-    je _exit        ;If this is the last argument exit
+    mov edx,eax     ; String length
+    mov ecx,ebx     ; String
+    mov ebx,1       ; stdout
+    mov eax,4       ; sys_write
+    int 0x80        ; Kernel interrupt
+    cmp ebp,0       ; Here we need to add a conditional to check if either the stack is empty or we've processed all arguments
+    je _exit        ; If this is the last argument exit
 
     mov edx,1
-    mov ecx,space   ;empty space
-    mov ebx,1       ;stdout
-    mov eax,4       ;sys_write
-    int 0x80        ;Kernel interrupt
+    mov ecx,space   ; Empty space
+    mov ebx,1       ; stdout
+    mov eax,4       ; sys_write
+    int 0x80        ; Kernel interrupt
 
     pop	ebx		      ; Get argument
     jmp _main
@@ -54,10 +54,10 @@ _main:
 _exit:
     ;Print new line
     mov edx,1
-    mov ecx,newline ;New line
-    mov ebx,1       ;stdout
-    mov eax,4       ;sys_write
-    int 0x80        ;Kernel interrupt
+    mov ecx,newline ; New line
+    mov ebx,1       ; stdout
+    mov eax,4       ; sys_write
+    int 0x80        ; Kernel interrupt
 
     ;Exit with code 0
     mov eax, 1
