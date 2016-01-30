8a7e9547e3a18082b902a4a5cf163128dcbcb87a
faissaloo <faissaloo@gmail.com>
2016-01-30 10:20:27 +0000

    Used faster strlen method

--- echo.asm
+++ echo.asm
@@ -22,28 +22,53 @@ _removenl:
     pop ebx
 _main:
     ;strlen(edi)
+    ;Here we have an assembly implementation of glibc's strlen.c
+    ;Yes, that's right, I'm using *that* method because it's REALLY fast
     mov edi, ebx
     ;Get the string length for string edi and put it in eax
     push	edi
   	xor	ecx, ecx
   	mov	edi, [esp]
-  	not	ecx
   	xor	al, al
-    repne scasb
-  	not	ecx
-  	pop	edi
-  	lea	eax, [ecx-1]
-    mov byte [ebx+eax],32 ; Put a space in between each argument to replace the string terminator
+_s: scasw
+    mov ecx,[edi]
+    sub ecx, 0x01010101
+    and ecx, 0x80808080
+    xor ecx, 0  ;compare ecx with 0
+    jz _s ;If none of them were zeros loops back to s
+    ;otherwise let's track down the one that was zero which will be represented a 0x80
+    cmp ecx, 0x80
+    je _cont
+
+    shr ecx, 8 ; "NEXT!"
+    inc edi
+    cmp ecx, 0x80
+    je _cont
+
+    shr ecx, 8
+    inc edi
+    cmp ecx, 0x80
+    je _cont
+
+    shr ecx, 8
+    inc edi
+    cmp ecx, 0x80
+    je _cont
+_cont:
+    mov ecx,ebx ;Save the original starting point in ecx, we don't want to modify ebx
+    sub ecx, edi  ;Get the difference
+    neg ecx ;It's going to be negative so make it positive
+    mov byte [ebx+ecx],32 ; Put a space in between each argument to replace the string terminator
     dec esi          ; Decrease arg count
     jnz _main        ; If this is the last argument exit
 
 
 _exit:
     ;Append a newline to the end if we have a newline
-    mov [ebx+eax],edx
-    inc eax ;Increase the length by one
+    mov [ebx+ecx],edx
+    inc ecx ;Increase the length by one
     ; Print the string
-    mov edx,eax     ; String length
+    mov edx,ecx     ; String length
     mov ecx,ebx     ; String
     mov ebx,1       ; stdout
     mov eax,4       ; sys_write
