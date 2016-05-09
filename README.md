9329cff02a5aac21f19e6bf3bb877f23e3d36191
Peter Ferrie <peter.ferrie@gmail.com>
2016-05-09 13:41:17 -0700

    major size reduction

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 echo
 ===
-This is a rewrite of GNU's 'echo' program (/bin/echo) in Assembly that is 99.2% smaller (240 bytes vs 31296 bytes). It only has one argument, '-n', which removes the newline at the end.
+This is a rewrite of GNU's 'echo' program (/bin/echo) in Assembly that is 99.4% smaller (184 bytes vs 31296 bytes). It only has one argument, '-n', which removes the newline at the end.
--- echo.asm
+++ echo.asm
@@ -34,87 +34,76 @@ phdr:                                                 ; Elf32_Phdr
 phdrsize      equ     $ - phdr
 
 _start:
-    pop	esi	        ; Get the number of arguments
+    push `\n`     ;The newline character
+    pop eax
+    pop	ebx	        ; Get the number of arguments
 
-    dec esi      ; If there are no arguments just exit instantly
+    dec ebx      ; If there are no arguments just exit instantly
     jz _noArgsExit
 
     pop	ecx	        ; Pop the program name, we don't need this so we'll just overwrite it
 
-    pop	ecx		      ; Get first argument
-    mov ebx, 0x80808080 ;Store the himagic in ebx so we can speed things up a little
-    ;Listen: I don't give a damn if you don't like the fact that I'm using a non
-    ;general purpose register for this, it doesn't get used and it's much faster
-    mov ebp, 0x7F7F7F7F
-    ;compare ecx with '-n' to see if they're the same
-    mov ah,`\n`     ;The newline character
-    mov edx, [ecx]
-    and edx, 0xFFFFFF ; Mask the bits we don't need
-    cmp edx,`-n\0`;Check for '-n'
+    pop	edi		; Get first argument
+    ;compare edx with '-n' to see if they're the same
+    mov edx, [edi]
+    bswap edx
+    xor dl, dl          ; Mask the bits we don't need
+    sub edx,`\0\0n-`;Check for '-n'
     jne _main
 _removenl:
-    xor ah,ah ;Removes the newline character from memory
-    xor edx,edx ;Set edx back to zero from before to prevent segfaults
-    dec esi
+    xchg edx, eax       ; Removes the newline character from memory
+    push edi
+    dec ebx
     jz _exit
-    pop ecx
+    pop edi
+    pop edi
 _main:
-    ;strlen(edx)
-    ;Here we have an assembly implementation of glibc's strlen.c
-    ;Yes, that's right, I'm using *that* method because it's REALLY fast
-    mov edx, ecx
-    ;Get the string length for string edx and put it in eax
+    ;strlen(edi)
+    push edi
+    ;Get the string length for string edi and put it in edi
+    db 0x3c             ;Mask scasd on first pass
 _s:
-    mov edi,[edx]
-    add edx,4     ;Move to the next 'double word' (because we'll be decreasing from it)
+    scasd               ;Move to the next 'double word'
+_sit:
+    mov edx,[edi]
     ; Wooo magical numbers!
-    and edi, ebp
-    sub edi, 0x01010101
-    and edi, ebx
-    xor edi, 0  ;compare edi with 0
+    sub edx, 0x01010101
+    and edx, 0x80808080  
     jz _s ;If none of them were zeros loops back to s
     ;otherwise let's track down the one that was zero which will be represented a 0x80
-    sub edx, 4      ;Remove the 'add edx, 2' that we did before
-    ; mov edx again, so that we can test the actual value, since our cool magic
-    ; number stuff that we did before destroys edx, which means that character
-    ; 128 will cause misfires
-    mov edi,[edx]
-    test edi, 0xFF
-    jz _cont
+    test dl, dl
+    jnz _cont
 
+    test dh, dh
+    jnz _cont1
 
-    inc edx
-    test edi, 0xFF00
-    jz _cont
+    bswap edx
+    test dh, dh
+    jnz _cont2
 
+    inc edi
 
-    inc edx
-    test edi, 0xFF0000
-    jz _cont
+_cont2:
+    inc edi
 
-    inc edx
-    test edi, 0xFF000000
-    jnz _s ;If it was a misfire, go back and continue
+_cont1:
+    inc edi
 
 _cont:
-    mov edi,ecx ;Save the original starting point in edi, we don't want to modify ecx
-    sub edx, edi  ;Get the difference
-    mov byte [ecx+edx],32 ; Put a space in between each argument to replace the string terminator
-    dec esi          ; Decrease arg count
-    jnz _main        ; If this is the last argument exit
+    mov byte [edi], 32 ; Put a space in between each argument to replace the string terminator
+    dec ebx          ; Decrease arg count
+    jnz _sit         ; If this is the last argument exit
 
 
 _exit:
     ;Append a newline to the end if we have a newline
-    mov [ecx+edx],ah
-    inc edx ;Increase the length by one
+    stosb   ;and increase the length by one
     ; Print the string
-    ;String length should already be in edx
-    ;String should already be in ecx
-    ;mov edx,edx     ; String length
-    ;mov ecx,ecx     ; String
-    mov ebx,1       ; stdout
-    mov eax,4       ; sys_write
+    pop ecx          ; String
+    mov edx, edi
+    sub edx, ecx     ; String length
+    inc ebx          ; stdout
+    mov al,4         ; sys_write
     push _sysentercont
     push ecx
     push edx
@@ -123,9 +112,9 @@ _exit:
     sysenter        ; Kernel interrupt
 _sysentercont:
     ;Exit with code 0
-    xor ebx, ebx
+    dec ebx
 _noArgsExit:
-    mov eax, 1
+    mov al, 1
     mov ebp, esp
     sysenter
 
